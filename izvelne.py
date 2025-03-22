c29d025e398c5b8a4224e8f9e9f9cbd967057de1
Arturs-gailitis <artursgailitis2003@gmail.com>
2025-03-22 15:54:54 +0200

    lai nebūtu jāvirza pa horizontāli

--- izvelne.py
+++ izvelne.py
@@ -16,7 +16,8 @@ iestatījumi(root, 500, 400, 'Programmas par viļņu zīmēšanu galvenā izveln
 fons(root, 500, 400, "fona_attēls.jpg")
 
 #Label, kas lietotājam liek saprast par programmu
-par_pogām_tekts = tk.Label(root, text='Izvēlaties ar kādu funkciju attēlosiet viļņus', font=('Ariel', 14), background='lightblue')
+par_pogām_tekts = tk.Label(root, text='Izvēlaties ar kādu funkciju attēlosiet viļņus', font=('Ariel', 14), 
+                           background='lightblue')
 par_pogām_tekts.pack(pady=10)
 
 #Poga uz sinusoīdo viļņu funkcijas izveidošanu
@@ -24,11 +25,13 @@ sīnusa_poga = tk.Button(root, text='Sinusoīdo viļņu funkcija', background='l
 sīnusa_poga.pack(pady=10)
 
 #Poga uz kosinusoidālā viļņu funkcijas izveidošanu
-kosīnusa_poga = tk.Button(root, text='Kosinusoidālā viļņa funkcija', background='lightblue', command=lambda: c_dati(root))
+kosīnusa_poga = tk.Button(root, text='Kosinusoidālā viļņa funkcija', background='lightblue', 
+                          command=lambda: c_dati(root))
 kosīnusa_poga.pack(pady=10)
 
 #Poga uz kvadrātveida sinusoidālo viļņa funkciju izveidošanas
-kvadrāt_poga = tk.Button(root, text='Kvadrātveida sinusoidālo viļņa funkcija', background='lightblue', command=lambda: k_dati(root))
+kvadrāt_poga = tk.Button(root, text='Kvadrātveida sinusoidālo viļņa funkcija', background='lightblue', 
+                         command=lambda: k_dati(root))
 kvadrāt_poga.pack(pady=10)
 
 #Poga uz kvadrātveida sinusoidālo viļņa funkciju izveidošanas
@@ -36,7 +39,8 @@ gauss_poga = tk.Button(root, text='Gaussa viļņa funkcija', background='lightbl
 gauss_poga.pack(pady=10)
 
 #Poga ar kuru var izslēgt programmu
-beigu_poga = tk.Button(root, text='Programmas beigas', command=lambda: programmas_beigas(root), background='lightblue')
+beigu_poga = tk.Button(root, text='Programmas beigas', command=lambda: programmas_beigas(root), 
+                       background='lightblue')
 beigu_poga.pack(pady=10)
 
 root.mainloop()
