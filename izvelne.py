c9a563b05cb84df4d470372d46f503640936ed5d
Arturs-gailitis <artursgailitis2003@gmail.com>
2025-03-22 11:05:05 +0200

    kosinusoidālā viļņu funkcija

--- izvelne.py
+++ izvelne.py
@@ -3,6 +3,7 @@ import tkinter as tk
 from programmas_iestatījumi import iestatījumi, programmas_beigas, fons
 from sin_dati import s_dati
 from cos_dati import c_dati
+from kvadratveids import k_dati
 
 #Izveido galveno grafisko logu
 root = tk.Tk()
@@ -24,6 +25,10 @@ sīnusa_poga.pack(pady=10)
 #Poga uz kosinusoidālā viļņu funkcijas izveidošanu
 kosīnusa_poga = tk.Button(root, text='Kosinusoidālā viļņa funkcija', background='lightblue', command=lambda: c_dati(root))
 kosīnusa_poga.pack(pady=10)
+
+#Poga uz kvadrātveida sinusoidālo viļņa funkciju izveidošanas
+kvadrāt_poga = tk.Button(root, text='Kvadrātveida sinusoidālo viļņa funkcija', background='lightblue', command=lambda: k_dati(root))
+kvadrāt_poga.pack(pady=10)
 
 #Poga ar kuru var izslēgt programmu
 beigu_poga = tk.Button(root, text='Programmas beigas', command=lambda: programmas_beigas(root), background='lightblue')
---
+++ kvadratveids.py
@@ -0,0 +1,77 @@
+import tkinter as tk
+import numpy as np
+import matplotlib.pyplot as plt
+from programmas_iestatījumi import iestatījumi, fons
+
+def k_dati(root):
+    
+    logs = tk.Toplevel(root) # Tiek atvērta jauns apakšlogs
+
+    iestatījumi(logs, 500, 400, 'Ievada mainīgo vērtības priekš kvadrātveida viļņu funkcijas')
+
+    fons(logs, 500, 400, "fona_attēls1.jpeg")
+
+    # Tiek izveidotas kolonnas un rindas, kuros tiks ielikti elementi, lai tie izskatītos kārtīgi
+    logs.columnconfigure(0, weight=1) 
+    logs.columnconfigure(1, weight=1)
+
+    logs.rowconfigure(0, weight=1)
+    logs.rowconfigure(1, weight=1)
+    logs.rowconfigure(2, weight=1)
+    logs.rowconfigure(3, weight=1)
+    logs.rowconfigure(4, weight=1)
+
+    #T iek izveidoti Label, kas paskaidro kādas vērtības ir domātas noteiktajiem texta logiem 
+    tk.Label(logs, text='Amplitūde', background='darkgrey').grid(row=0, column=0, padx=10, pady=5, sticky="e")
+    tk.Label(logs, text='Frekvence', background='darkgrey').grid(row=1, column=0, padx=10, pady=5, sticky="e")
+    tk.Label(logs, text='X vērtība', background='darkgrey').grid(row=2, column=0, padx=10, pady=5, sticky="e")
+    tk.Label(logs, text='Viļņa izplatīšanās ātrums',
+              background='darkgrey').grid(row=3, column=0, padx=10, pady=5, sticky="e")
+
+    # Lietotājs raksta iekšā teksta logos vērtības
+    amplitūds_ievades = tk.Entry(logs, width=10)
+    amplitūds_ievades.grid(row=0, column=1, padx=10, pady=5, sticky="w")
+
+    frekvence_ievades = tk.Entry(logs, width=10)
+    frekvence_ievades.grid(row=1, column=1, padx=10, pady=5, sticky="w")
+
+    x_ievades = tk.Entry(logs, width=10)
+    x_ievades.grid(row=2, column=1, padx=10, pady=5, sticky="w")
+
+    ātruma_ievades = tk.Entry(logs, width=10)
+    ātruma_ievades.grid(row=3, column=1, padx=10, pady=5, sticky="w")
+
+    #Tiek radīta poga, kas apkopos uz aizsūtīs vērtības uz grafikas zīmēšanu
+    vērtību_ievade = tk.Button(logs, text='Ievada vērtības',
+                               command=lambda: saglabā_un_zīmē_k(amplitūds_ievades, frekvence_ievades, 
+                                                                x_ievades, ātruma_ievades), background='darkgrey')
+    vērtību_ievade.grid(row=4, column=0, columnspan=2, pady=10)
+
+def saglabā_un_zīmē_k(amplitūds_ievades, frekvence_ievades, x_ievades, ātruma_ievades):
+    
+    #Nepieciešamās vērtības
+    amplitūde = float(amplitūds_ievades.get()) # Amplitūde
+    frekvence = float(frekvence_ievades.get()) # Viļņa frekvence
+    ātrums = float(ātruma_ievades.get()) # Viļņa izplatīšanās ātrums
+    x_v = int(x_ievades.get()) # x vērtības
+
+    k = (2 * np.pi) * frekvence / ātrums  # Viļņu skaits
+
+    x = np.linspace(0, 10, x_v)  # X vērtību masīvs
+    y = amplitūde * np.sign(np.sin(k * x))  # Aprēķina kvadrātveida sīnusa viļņa vērtības
+
+    #Uzzīmē grafiku
+    plt.figure()
+
+    plt.plot(x, y)
+
+    plt.xlabel("X pozīcija")
+    plt.ylabel("Amplitūda")
+
+    plt.title("Vilnis kurš ir veidots ar kvadrātveida sinusoidālo funkciju")
+
+    plt.legend()
+
+    plt.grid()
+
+    plt.show()
